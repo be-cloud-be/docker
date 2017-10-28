b7242cb6ff4fea5bc51c327528e392d97cca521c
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2017-10-28 08:05:43 +0000

    patch odoo for postgres user

--- 11.0/entrypoint.sh
+++ 11.0/entrypoint.sh
@@ -8,7 +8,7 @@ regex="^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$"
 export HOST=${BASH_REMATCH[3]}
 export PORT=${BASH_REMATCH[4]}
 export NAME=${BASH_REMATCH[5]}
-export USER=${BASH_REMATCH[1]}
+#export USER=${BASH_REMATCH[1]}
 export PASSWORD=${BASH_REMATCH[2]}
 
 # set the postgres database host, port, user and password according to the environment
