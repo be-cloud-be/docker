02076b04404873cf1d09aced6fafc3b398af5ddc
Jerome Sonnet (be-cloud.be) <jerome.sonnet@imply.lu>
2017-10-28 15:14:25 +0000

    nginx parameters

--- 11.0/entrypoint.sh
+++ 11.0/entrypoint.sh
@@ -11,8 +11,6 @@ export NAME=${BASH_REMATCH[5]}
 export USER=${BASH_REMATCH[1]}
 export PASSWORD=${BASH_REMATCH[2]}
 
-printenv
-
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
